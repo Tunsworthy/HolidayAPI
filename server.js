bfb00f678a4eed295719c5b7babca0a7648398a5
Tom Unsworth <mail@tomunsworth.net>
2019-09-22 22:19:48 +0100

    update auth

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(auth);
+//app.use(auth);
 routes(app);
 locations(app);
 checkin(app);
